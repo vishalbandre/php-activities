785c86d154769a4a8e38962c79079299db0b5e76
vishalbandre <iamvshlbndr@gmail.com>
2022-01-28 18:38:02 +0530

    Activity 7: Dependency Injection Done

---
+++ di_demo/initial/app.php
@@ -0,0 +1,15 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Pimple\Container;
+
+$container = new Container();
+require __DIR__ . '/config.php';
+require __DIR__ . '/services.php';
+
+/** 
+ * @var \DependencyInjectionDemo\SubscriberManager $subscriberManager
+ */
+$subscriberManager = $container['subscriberManager'];
+$subscriberManager->notifySubscribers();
---
+++ di_demo/initial/vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '0744c6d6a3a8cb4945131df5c605b34397aca178',
+        'name' => '__root__',
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '0744c6d6a3a8cb4945131df5c605b34397aca178',
+            'dev_requirement' => false,
+        ),
+        'pimple/pimple' => array(
+            'pretty_version' => 'v3.5.0',
+            'version' => '3.5.0.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../pimple/pimple',
+            'aliases' => array(),
+            'reference' => 'a94b3a4db7fb774b3d78dad2315ddc07629e1bed',
+            'dev_requirement' => false,
+        ),
+        'psr/container' => array(
+            'pretty_version' => '2.0.2',
+            'version' => '2.0.2.0',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../psr/container',
+            'aliases' => array(),
+            'reference' => 'c71ecc56dfe541dbd90c5360474fbc405f8d5963',
+            'dev_requirement' => false,
+        ),
+    ),
+);
